b9e1d327bf9de75ea1946af4c0178a2401f1c2f2
AreURdy4TheRock <dorkwingdock@gmail.com>
2023-04-12 21:22:10 +0300

    Добавьте файлы проекта.

---
+++ Module10/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10
+{
+    public class Calculator : ICalculator
+    {
+        static ILogger ILogger = new Logger();
+        public int Calc(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Input()
+        {
+            int number;
+            while (true)
+            {
+                try
+                {
+                    number = Int32.Parse(Console.ReadLine());
+                    return number;
+                }
+                catch (Exception ex)
+                {
+                    ILogger.Error("Введите корректное число:");
+                }
+            }
+
+        }
+    }
+}
---
+++ Module10/Program.cs
@@ -0,0 +1,48 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Module10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger Logger = new Logger();
+            ICalculator calculator = new Calculator();
+            Logger.Event("Введите первое число:"); 
+            int a = Calculator.Input();
+            Logger.Event("Введите второе число:");
+            int b = Calculator.Input();
+            Logger.Event($"Результат: {calculator.Calc(a, b)}");
+        }
+    }
+
+    public interface ICalculator
+    {
+        int Calc(int a, int b) 
+        { 
+            return a + b; 
+        }
+
+    }
+
+    public interface ILogger
+    {
+        public void Event(string message);
+        public void Error(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+        }
+    }
+}
